55f2781a9bc159d76945d5ca3bf8150dad90c8c7
Andrés Berdugo <andres700crac@gmail.com>
2024-05-20 18:37:09 -0500

    add folder Routes and file StackNavigation.js

---
+++ routes/StackNavigation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import 'react-native-gesture-handler'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+const StackNavigation = () => {
+  return (
+    <NavigationContainer>
+        <StackNavigation>
+            
+        </StackNavigation>
+    </NavigationContainer>
+  )
+}
+
+export default StackNavigation
