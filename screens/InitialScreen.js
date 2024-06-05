f838b7b68dd0582d4fb21d84002ab9aef3ae184a
Andrés Berdugo <andres700crac@gmail.com>
2024-06-05 15:23:32 -0500

    feat: created new folder modals AddFriendComponent SearchbarComponent and change screens and TabNavigation

--- screens/InitialScreen.js
+++ screens/InitialScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, Text, Touchable, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
 
 const InitialScreen = ({navigation}) => {
 
